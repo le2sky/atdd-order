45384ee962198b050ecb259faac3c698616ee3a5
le2sky <leehaneul990623@gmail.com>
2024-04-28 14:37:43 +0900

    chore: initial commit

---
+++ src/main/java/com/example/atddorder/AtddOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.atddorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtddOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtddOrderApplication.class, args);
+    }
+
+}
